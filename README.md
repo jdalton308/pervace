4b281526b4783ab4c438ccdf7baea5a6252b9c2e
jdalton308 <jdalton308@yahoo.com>
2016-02-10 17:33:59 -0800

    Initial commit: File structure taken from kindling

---
+++ README.md
@@ -0,0 +1,2 @@
+###Description
+This is a prototype of a web application that collects and presents facial expressions of anyone who visits the page.
---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var less = require('gulp-less');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var htmlmin = require('gulp-htmlmin');
+
+var styleFiles = [
+        './src/less/main.less'
+    ];
+var jsFiles = [
+		'node_modules/jquery/dist/jquery.js',
+		'./src/js/**/*.js'
+	];
+var htmlFiles = [
+		'./*.html'
+	];
+
+
+gulp.task('html', function() {
+	gulp.src('src/*.html')
+		.pipe(htmlmin({collapseWhitespace: true}))
+		.pipe(gulp.dest('./build/'))
+});
+
+gulp.task('styles', function() {
+    gulp.src(styleFiles)
+        .pipe(concat('app.css'))
+        .pipe(less())
+        .pipe(gulp.dest('./build/css/'));
+});
+
+gulp.task('scripts', function(){
+	gulp.src(jsFiles)
+		.pipe(concat('app.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('./build/js/'))
+		.on('error', gutil.log);
+});
+
+gulp.task('watch', function(){
+    gulp.watch(['./src/less/**/*.less'], ['styles']);
+    gulp.watch(['./src/js/**/*.js'], ['scripts']);
+});
+
+gulp.task('build', ['html', 'styles', 'scripts'])
+
+gulp.task('default', ['build', 'watch']);
